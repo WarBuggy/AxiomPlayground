174f8011a123df3ed519f2d44876ca54dc4da691
WarBuggy <hovanbuu@gmail.com>
2026-02-03 14:05:38 -0800

    * ScriptManager: remove function CreateATable.

--- src/Scripting/ScriptManager.cs
+++ src/Scripting/ScriptManager.cs
@@ -289,11 +289,6 @@ public sealed class ScriptManager
                 : DynValue.FromObject(_luaScript, a))];
     }
 
-    public Table CreateATable()
-    {
-        return new Table(_luaScript);
-    }
-
     public sealed class ScriptQueueItem
     {
         public required string ModId { get; set; }
